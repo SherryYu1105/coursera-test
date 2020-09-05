825382aeb0c1dcfae3ff5d3e683ac194e8944d8d
YuShuting <yushuting115@outlook.com>
2020-09-06 00:04:29 +0800

    some change done

--- module4/SpeakGoodBye.js
+++ module4/SpeakGoodBye.js
@@ -2,6 +2,9 @@
     var byeSpeaker = {}; 	
   byeSpeaker.speak = function (name) {  console.log(speakWord + " " + name); 	
  }  
+
+ var speakWord = "Good Bye";
+ 
  function speak(name) {  console.log(speakWord + " " + name); }  
    window.byeSpeaker = byeSpeaker; 
  })(window);  
--- module4/SpeakHello.js
+++ module4/SpeakHello.js
@@ -2,6 +2,7 @@
     var helloSpeaker = {}; 	
      helloSpeaker.speak = function (name) { console.log(speakWord + " " + name); 	
   }  
+  var speakWord = "Hello"
   function speak(name){ console.log (speakWord + " " + name);}
   window.helloSpeaker=helloSpeaker;
   })(window);
